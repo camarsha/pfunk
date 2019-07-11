3d08aa0620b758e75ebdecdc22f0420159c7aaed
Caleb <camarsha@ncsu.edu>
2019-07-11 13:40:21 -0400

    Trying to setup a package.

---
+++ Setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='P-Funk',
+      version=0.1,
+      description='Python package for Fresco UNcertainty Kalculations',
+      author='Caleb Marshall',
+      author_email='blah@nofuckingway.hotmail',
+      url='https://github.com/dubiousbreakfast/MCMC-FRESCO',
+      packages=['src'],
+     )
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+import src.FrescoClasses 
+import src.FrescoFit
+import src.Model
+import src.Sampler
