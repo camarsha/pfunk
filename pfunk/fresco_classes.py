bebc0b3ef327d1ec81844bc392750835d1a3eeca
caleb <camarsha@ncsu.edu>
2020-11-17 19:36:41 -0500

    Documentation update

--- pfunk/fresco_classes.py
+++ pfunk/fresco_classes.py
@@ -1,183 +1,125 @@
 import numpy as np
-import re
 import os
-from collections import OrderedDict
 import pandas as pd
-
-############################################
-###############Functions####################
-############################################
-
 
 
 def filerun(filename):
     """filerun(filename)
     Runs a fresco input file. Output is piped into /dev/null
     in order to avoid writing output file.
-    
-    :param filename: name of the fresco input file to run 
+
+    :param filename: name of the fresco input file to run
     :returns: None
-    :rtype: None
 
     """
-    command = 'fresco' + '<' + filename + '> /dev/null' 
+    command = 'fresco' + '<' + filename + '> /dev/null'
     os.system(command)
 
-        
-# Reads fort.200 files returns LineObject
-def read_cross(filename):
-    """FIXME! briefly describe function
 
-    :param filename: 
-    :returns: 
-    :rtype: 
+def read_cross(filename):
+    """ Reads fort.200 files from FRESCO returns
+    instance of LineObject.
+
+    :param filename: Name of fort.20* file to read.
+    :returns: instance of LineObject
 
     """
+
     cross = pd.read_csv(filename, sep='\s+', skiprows=10, header=None)
     theta = cross[cross.columns[0]].tolist()[:-1]
     sigma = cross[cross.columns[1]].tolist()[:-1]
-    E = 0.0
-    graphline = LineObject(theta,sigma,E,'0','+')
+    graphline = LineObject(theta, sigma)
     return graphline
 
-# Reads two col. data files returns DataObject
-def read_data(filename,delim=None):
+
+def read_data(filename, delim=None):
+    """ Reads col. data files returns DataObject.
+    Expected format is:
+
+
+    Angles   Cross Sections   Uncertainties
+    5.0      10.0             0.2
+    etc.
+
+    :param filename: Name of the data file
+    :param delim: The character that separates the columns
+    :returns: instance of DataObject
+
+    """
     if delim:
         data = pd.read_csv(filename, sep=delim)
 
     else:
         data = pd.read_csv(filename, sep='\s+')
+
     theta = data[data.columns[0]].tolist()
     sigma = data[data.columns[1]].tolist()
+
     try:
-        erry =  data[data.columns[2]]
+        erry = data[data.columns[2]]
         erry[erry == 0.0] = 1.e-6
         erry = erry.tolist()
     except IndexError:
         erry = None
-    graphline = DataObject(theta,sigma,erry)
+
+    graphline = DataObject(theta, sigma, erry)
     return graphline
 
-#Reads fort 17 file and returns a wavefunction class object to plot
-#def read_wavefunction(filename):
 
-
-#do a sum of two cross sections that have the same angles
-def cs_sum(cs1,cs2):
-    if not cs1.theta == cs2.theta:
-        print("These cross sections don't have the same angles!")
-    else:
-        return LineObject(cs1.theta,cs1.sigma+cs2.sigma,cs1.E,cs1.J,cs1.par)
-
-
-
-
-
-############################################
-#########Classes For Plotting###############
-############################################
-
-#new generic class for angular distrubutions
 class Angles():
-    def __init__(self,theta,sigma):
+    def __init__(self, theta, sigma):
+        """Basic class that defines differential cross
+        sections.
+
+        :param theta: :code:`np.array` of angles
+        :param sigma: :code:`np.array` of differential cross sections
+
+        """
         self.theta = np.asarray(theta, dtype='float64')
         self.sigma = np.asarray(sigma, dtype='float64')
 
 
-#This is the tenenative class for graphs. It includes scaling for elastic fits.
 class LineObject(Angles):
-    def __init__ (self,theta,sigma,E,J,par):
-        self.E = E
-        self.J = J
-        self.par = par
-        Angles.__init__(self,theta,sigma)
+    def __init__(self, theta, sigma):
+        """Class intended for theoretically calculated
+        cross sections. At this time, it has no
+        additional features when compared to :class:`Angles`.
 
-    def scale_it(self,value,angle,constant=None):
-        if constant:
-            self.sigma[:] = value*self.sigma
-        else:
-            index = self.find_angle(angle)
-            scale = value/self.sigma[index]
-            print('Factor is: ', scale) 
-            #Added slice overwrite to be a bit more careful with list
-            self.sigma[:] = scale*self.sigma
+        :param theta: :code:`np.array` of angles
+        :param sigma: :code:`np.array` of differential cross sections
 
-            
-    #Picks out list index for a given angle.
-    def find_angle(self,angle):
-        angle = float(angle)
-        if angle in self.theta:
-            return np.argwhere(self.theta == angle) 
-        
-        else:
-            angle = float(input('Angle not found try again! \n'))
-            print(self.theta)
-            self.find_angle(angle)
+        """
+        Angles.__init__(self, theta, sigma)
 
-    #resize angles
-    def angle_range(self,angle):
-        index = self.find_angle(angle)+1 #slicing is exclusive, so we need to add one
-        return (self.theta[0:index],self.sigma[0:index])
-        
-    #function for angle ref Ian Thompsons's book written in a form for root finding since it is transendental
-    def com_fun(self,x,a,b):
-        return (np.tan(a) - (np.sin(x)/(b+np.cos(x))))
 
-    #function for cross section
-    def cross_factor(self,com_angle,rho):
-        return ((1+rho**2+2*rho*np.cos(com_angle))**(3.0/2.0))/(np.abs(1+rho*np.cos(com_angle)))
+class DataObject(Angles):
+    def __init__(self, theta, sigma, erry):
+        """
 
-    #lab angle transformation
-    def lab_fun(self,a,b):
-        return (np.arctan((np.sin(a)/(b+np.cos(a)))))
+        :param theta: :code:`np.array` of angles
+        :param sigma: :code:`np.array` of differential cross sections
+        :param erry: :code:`np.array` of cross section uncertainties
 
-    
-    #function to make angles positive
-    def make_positive(self,angle):
-        np.putmask(angle,angle<0.0,angle+180.0) #putmask is destructive
-        print(angle)
-        return angle
-    
-    #Transfers lab frame data to center of mass.
-    #Freaks out at 90 degrees as one might expect
-    def to_com(self,massa,massb,massc,massd,Elab,Q):
-        angle = self.theta*(np.pi/180.0) #to radians 
-        rho = np.sqrt((massa*massc)/(massd*massb)*Elab/(Elab+Q))
-        sol = optimize.fsolve(self.com_fun,angle,args=(angle,rho))
-        #now alter sigma
-        cs_scale = self.cross_factor(sol,rho) #solve transformation for cross section
-        com_angle = sol*(180.0/np.pi) #to degrees
-        com_angle = self.make_positive(com_angle)
-        self.theta = com_angle
-        self.sigma = cs_scale*self.sigma
-
-    #from com to lab
-    def to_lab(self,massa,massb,massc,massd,Elab,Q):
-        angle = self.theta*(np.pi/180.0)
-        rho = np.sqrt((massa*massc)/(massd*massb)*Elab/(Elab+Q))
-        lab_scale = (self.cross_factor(angle,rho))**(-1.0) #uses com angles for scale
-        lab_angle = self.lab_fun(angle,rho)*(180.0/np.pi)
-        lab_angle = self.make_positive(lab_angle)
-        self.theta = lab_angle
-        self.sigma = lab_scale*self.sigma
-        
-#new subclass for experimental data.
-class DataObject(LineObject):
-    def __init__(self,theta,sigma,erry):
-        Angles.__init__(self,theta,sigma)
-        #We do not expect all data files to have errors
+        """
+        Angles.__init__(self, theta, sigma)
         self.erry = np.asarray(erry)
 
 
 class NamelistInput():
 
     """
-    This assumes variables have been laid out line by line. We can simply define
-    which lines to alter, and hopefully save time.
+    Class that keeps track of a FRESCO input file. File is assumed to
+    have one variable per line, which can then be specified and changed,
     """
 
     def __init__(self, filename):
 
+        """
+        Read the file into an array. Each array element is
+        one variable.
+
+        :param filename: fresco input file
+        """
         self.initial_file = []
 
         with open(filename, 'r') as f:
@@ -189,6 +131,13 @@ class NamelistInput():
         self.names = []
 
     def unpacklist(self, alist):
+        """ Create a list that expands all elements
+        that are tuples.
+
+        :param alist: input list
+        :returns: new list
+
+        """
         new_list = []
         for ele in alist:
             if isinstance(ele, tuple):
@@ -199,51 +148,75 @@ class NamelistInput():
         return new_list
 
     def find_tuples(self, alist):
-        # Just create a list with all the tuple indices, i.e duplicate values.
+        """ Create a list with the indices of the tuples, i.e duplicate values.
+        This list is stored as a member variable :param:`self.dup_size`
+
+        :param alist: list that contains tuples
+
+        """
+
         self.dup_size = [len(i) if isinstance(i, tuple) else 1 for i in alist]
 
     def create_names_positions(self, names, positions):
+        """This method initializes all of the information
+        associated with the parameter names and locations.
+        Importantly it handles all of the tuples.
+
+        :param names: list of all FRESCO variable names
+        :param positions: list of array indices to find variables
+
+        """
         self.find_tuples(positions)
         self.names = self.unpacklist(names)
-        self.name_positions = self.unpacklist(positions)                        
-        self.unique_name_positions = [i[0] if isinstance(i, tuple) else i for i in positions]
+        self.name_positions = self.unpacklist(positions)
+        self.unique_name_positions = [i[0] if isinstance(i, tuple)
+                                      else i for i in positions]
 
     def transform_values(self, values):
-                                
-        """
-        Transforms a array of n values to 
+        """ Transforms an array of n values to
         one of m values, where m is the length of
         the vector including the duplicated/shared values defined before.
+
+        :param values: np.array of n values
+        :returns: np.array of m values
+
         """
-        m_values = [np.repeat(i, j) for i,j in zip(values, self.dup_size)]
+
+        m_values = [np.repeat(i, j) for i, j in zip(values, self.dup_size)]
         return np.concatenate(m_values)  # Flatten everything
-            
+
     def swap_values(self, values):
-        """
-        Replaces the variables in the array and writes to a new input file.
+        """Method that takes array of values from minimization or sampler
+        and generates a new FRESCO file to run.
+
+        :param values: np.array of new values
+        :returns: generates a new input file named "new_input"
+
         """
         values = self.transform_values(values)
         new_values = []
         # Construct the new strings to replace
         for i, j in zip(self.names, values):
-            new_values.append('    '+i+' = '+str(j)+'\n') # Format the string properly
-        self.new_file[self.name_positions] = new_values # Swap them
+            new_values.append('    '+i+' = '+str(j)+'\n')  # Format the string
+        self.new_file[self.name_positions] = new_values  # Swap them
         # Now write to file
         np.savetxt('new_input', self.new_file,
                    fmt=('%s'), newline='')
 
     def initial_values(self):
+        """ Find the initial values of the parameters that have been
+        assigned line numbers and names.
         """
-        Find the initial values of the parameters that have been assigned 
-        line numbers and names.
-        """
-        self.x0  = [] # Initial values 
+        self.x0 = []  # Initial values
         for ele in self.unique_name_positions:
             value = self.initial_file[ele].split('=')[1]
             self.x0.append(float(value))
+        self.x0 = np.asarray(self.x0)
 
     def create_original(self):
+        """ Generate "new_input" file that is
+        identical to the original input file.
+
+        """
         np.savetxt('new_input', self.initial_file,
                    fmt=('%s'), newline='')
-
-        
