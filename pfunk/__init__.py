c698efc4fe76282a2be64bcb8ba61990d12d609d
Caleb <marshallc@ohio.edu>
2025-06-09 10:09:56 -0400

    Remove partial width module.

--- pfunk/__init__.py
+++ pfunk/__init__.py
@@ -4,6 +4,12 @@ from . import model_fit
 from . import model
 from . import sampler
 from . import utilities
-from . import partial_width
 
-__all__ = ['fresco_classes', 'model_fit', 'model', 'sampler', 'utilities', 'talys_classes']
+__all__ = [
+    "fresco_classes",
+    "model_fit",
+    "model",
+    "sampler",
+    "utilities",
+    "talys_classes",
+]
